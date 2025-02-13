185904624b03e377926a97038b772d6b2a161182
senpy <fivoszan@email.com>
2025-02-13 18:15:55 +0200

    adapting to changes from numiphy

--- henon.cpp
+++ henon.cpp
@@ -1,10 +1,10 @@
-#include "pyode.hpp"
+#include "/home/senpy/Documents/numiphy/numiphy/odesolvers/odepack/pyode.hpp"
 
 
 using Tx = double;
 using Tf = vec::StackArray<double, 4>;
 
-Tf hhode(const Tx& t, const Tf& q, const Tx* args){
+Tf hhode(const Tx& t, const Tf& q, const std::vector<Tx>& args){
     return {q[2], q[3], -(std::pow(args[4], 2.)*q[0] + args[0]*(std::pow(q[1], 2.) + 3.*args[1]*std::pow(q[0], 2.) + 2.*args[2]*q[0]*q[1])), -(std::pow(args[5], 2.)*q[1] + args[0]*(2.*q[0]*q[1] + args[2]*std::pow(q[0], 2.) + 3.*args[3]*std::pow(q[1], 2.)))};
 }
 
@@ -20,24 +20,16 @@ class HenonHeilesOde : public PyOde<Tx, Tf> {
     public:
         HenonHeilesOde(): PyOde<Tx, Tf>(hhode) {}
 
-        const PyOdeResult<Tx> poincare_solve(const py::tuple& py_ics, const Tx& x, const Tx& dx, const Tx& err, py::str method, const int max_frames, py::tuple pyargs, const bool display) {
+        const PyOdeResult<Tx> poincare_solve(const py::tuple& py_ics, const Tx& x, const Tx& dx, const Tx& err, py::str method, const int max_frames, py::tuple pyargs) {
 
-            vec::HeapArray<Tx> args;
-            Tx x0;
-            ICS<Tx, Tf> ics;
-            size_t nd, nt;
+            const PyOdeArgs<Tx> pyparams = {py_ics, x, dx, err, method, max_frames, pyargs, py::none(), py::none()};
+            OdeArgs<Tx, Tf> ode_args = to_OdeArgs<Tx, Tf>(pyparams);
+            ode_args.getcond = getcond;
         
-            if (!pyargs.empty()){
-                args = toCPP_Array<vec::HeapArray<Tx>>(pyargs);
-            }
-            x0 = py_ics[0].cast<Tx>();
-            Tf f0 = toCPP_Array<Tf>(py_ics[1]);
-            ics = {x0, f0};
-        
-            OdeResult<Tx, Tf> res = ODE<Tx, Tf>::solve(ics, x, dx, err, method.cast<std::string>().c_str(), max_frames, &args, getcond, nullptr, display);
+            OdeResult<Tx, Tf> res = ODE<Tx, Tf>::solve(ode_args);
             vec::HeapArray<Tx> f_flat = flatten(res.f);
-            nd = res.f[0].size();
-            nt = res.f.size();
+            size_t nd = res.f[0].size();
+            size_t nt = res.f.size();
         
             PyOdeResult<Tx> odres{res.x, f_flat, to_numpy(res.x, {nt}), to_numpy(f_flat, {nt, nd}), res.diverges, res.runtime};
         
@@ -68,8 +60,7 @@ PYBIND11_MODULE(henon, m){
             py::arg("max_frames") = -1,
             py::arg("args") = py::tuple(),
             py::arg("getcond") = py::none(),
-            py::arg("breakcond") = py::none(),
-            py::arg("display") = false)
+            py::arg("breakcond") = py::none())
         .def("psolve", &HenonHeilesOde::poincare_solve,
             py::arg("ics"),
             py::arg("t"),
@@ -78,8 +69,7 @@ PYBIND11_MODULE(henon, m){
             py::arg("err") = 0.,
             py::arg("method") = py::str("RK4"),
             py::arg("max_frames") = -1,
-            py::arg("args") = py::tuple(),
-            py::arg("display") = false)
+            py::arg("args") = py::tuple())
         .def("copy", &HenonHeilesOde::newcopy);
 
 
